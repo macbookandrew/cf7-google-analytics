19e34618a59d8eae5fd994f8e340e2e478c8c097
Andrew Minion <andrew@andrewrminion.com>
2018-04-25 09:54:15 -0400

    add translation file

--- gruntfile.js
+++ gruntfile.js
@@ -68,6 +68,7 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown']);
+	grunt.registerTask( 'default', ['i18n', 'uglify', 'i18n', 'readme',]);
 
 	grunt.util.linefeed = '\n';
 
