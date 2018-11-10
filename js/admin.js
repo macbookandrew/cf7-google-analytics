95a99e106ec4fc87bdcbdcda60496f6eb566a2bb
Andrew Minion <andrew@andrewrminion.com>
2018-11-10 14:47:19 -0500

    reuse dismissable notice JS

--- js/admin.js
+++ js/admin.js
@@ -10,12 +10,13 @@
 	$(document).ready(function() {
 
 		/** Handle dismissable notices */
-		$(document).on('click', '.cf7-ga-notice-170 .notice-dismiss', function() {
+		$(document).on('click', '.cf7-ga-notice .notice-dismiss', function(e) {
+			var version = $(this).parents('.cf7-ga-notice').data('version');
 
 			$.ajax({
 				url: ajaxurl,
 				data: {
-					action: 'cf7_ga_dismiss_notice_170'
+					action: 'cf7_ga_dismiss_notice_' + version
 				}
 			});
 
