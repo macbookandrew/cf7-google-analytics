48b5e19f67905086ce5bf12032c0993b3646c698
Andrew Minion <andrew@andrewrminion.com>
2018-11-10 14:02:19 -0500

    add settings for which events are sent

--- inc/class-cf7-google-analytics.php
+++ inc/class-cf7-google-analytics.php
@@ -18,7 +18,65 @@ class CF7_Google_Analytics {
 	 *
 	 * @var string
 	 */
-	public $version = '1.7.5';
+	public $version = '1.8.0';
+
+	/**
+	 * Available actions.
+	 *
+	 * @var string
+	 */
+	protected $actions = array(
+		'invalid'     => array(
+			'name'        => 'Invalid',
+			'description' => 'An Ajax form submission has completed successfully, but mail hasn’t been sent because there are fields with invalid input.',
+		),
+		'spam'        => array(
+			'name'        => 'Spam',
+			'description' => 'An Ajax form submission has completed successfully, but mail hasn’t been sent because a possible spam activity has been detected.',
+		),
+		'mail_sent'   => array(
+			'name'        => 'Mail Sent',
+			'description' => 'An Ajax form submission has completed successfully, and mail has been sent.',
+		),
+		'mail_failed' => array(
+			'name'        => 'Mail Failed',
+			'description' => 'An Ajax form submission has completed successfully, but it has failed in sending mail.',
+		),
+		'sent'        => array(
+			'name'        => 'Sent',
+			'description' => '(Legacy behavior) An Ajax form submission has completed successfully, regardless of other incidents.',
+		),
+	);
+
+	/**
+	 * Send action options.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @var array
+	 */
+	protected $send_actions;
+
+	/**
+	 * Get options.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @param string $action Action to check.
+	 *
+	 * @return array
+	 */
+	public function get_send_actions( $action ) {
+		if ( ! isset( $this->send_actions ) ) {
+			$this->send_actions = get_option( 'cf7_ga_send_actions' );
+		}
+
+		if ( array_key_exists( $action, $this->send_actions ) ) {
+			return $this->send_actions[ $action ];
+		}
+
+		return false;
+	}
 
 	/**
 	 * Get this plugin’s directory URL
@@ -33,6 +91,7 @@ class CF7_Google_Analytics {
 	 * Load everything
 	 */
 	public function __construct() {
+
 		/** Check version and load the correct file */
 		$wpcf7 = get_option( 'wpcf7' );
 		if ( $wpcf7['version'] <= '4.7' ) {
@@ -44,15 +103,32 @@ class CF7_Google_Analytics {
 		/** Register backend assets */
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_backend_assets' ) );
 
+		/** Add settings page */
+		add_action( 'admin_init', array( $this, 'settings_api' ) );
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+
 		/** Add notice about v1.7.0 changes */
 		if ( is_admin() && get_option( 'cf7-ga-170-notice-dismissed' ) === false ) {
-			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+			add_action( 'admin_notices', array( $this, 'admin_notices_170' ) );
 			add_action(
-				'admin_enqueue_scripts', function() {
+				'admin_enqueue_scripts',
+				function() {
 					wp_enqueue_script( 'admin-cf7-ga' );
 				}
 			);
 			add_action( 'wp_ajax_cf7_ga_dismiss_notice_170', array( $this, 'cf7_ga_dismiss_notice_170' ) );
+		}
+
+		/** Add notice about v1.8.0 changes */
+		if ( is_admin() && get_option( 'cf7-ga-180-notice-dismissed' ) === false ) {
+			add_action( 'admin_notices', array( $this, 'admin_notices_180' ) );
+			add_action(
+				'admin_enqueue_scripts',
+				function() {
+					wp_enqueue_script( 'admin-cf7-ga' );
+				}
+			);
+			add_action( 'wp_ajax_cf7_ga_dismiss_notice_180', array( $this, 'cf7_ga_dismiss_notice_180' ) );
 		}
 	}
 
@@ -119,15 +195,172 @@ class CF7_Google_Analytics {
 	 * Enqueue backend assets
 	 */
 	public function enqueue_backend_assets() {
-		wp_register_script( 'admin-cf7-ga', $this->get_plugin_dir_url() . 'js/admin.js', array( 'jquery' ), $this->version, true );
+		wp_register_script( 'admin-cf7-ga', $this->get_plugin_dir_url() . 'js/admin.min.js', array( 'jquery' ), $this->version, true );
 	}
 
 	/**
-	 * Add admin notice about new tracking behavior
+	 * Add backend settings.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
 	 */
-	public function admin_notices() {
+	public function settings_api() {
+
+		// Register setting.
+		register_setting( 'cf7_ga', 'cf7_ga_send_actions' );
+
+		// Add the section.
+		add_settings_section(
+			'cf7_ga',
+			__( 'Contact Form 7 to Google Analytics', 'cf7_ga' ),
+			array( $this, 'settings_header' ),
+			'cf7_ga'
+		);
+
+		// Add the individual settings.
+		foreach ( $this->actions as $key => $value ) {
+			add_settings_field(
+				'cf7_ga_' . $key,
+				$value['name'],
+				array( $this, 'render_setting_' . $key ),
+				'cf7_ga',
+				'cf7_ga'
+			);
+		}
+
+	}
+
+	/**
+	 * Register options page.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function admin_menu() {
+		add_options_page( 'Contact Form 7 to Google Analytics Options', 'CF7 to GA', 'manage_options', 'cf7_ga', array( $this, 'options_page' ) );
+	}
+
+	/**
+	 * Display options page.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function options_page() {
+
+		// Check capabilities.
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		settings_errors( 'cf7_ga_messages' );
 		?>
-		<div class="notice notice-info cf7-ga-notice-170 is-dismissible">
+		<div class="wrap">
+		<form action="options.php" method="post">
+		<?php
+		settings_fields( 'cf7_ga' );
+		do_settings_sections( 'cf7_ga' );
+		submit_button();
+		?>
+		</form>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Display settings page header content.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function settings_header() {
+		?>
+		<p>Choose which actions you would like to send to Google Analytics:</p>
+		<?php
+	}
+
+	/**
+	 * Render setting field for invalid.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function render_setting_invalid() {
+		$action = 'invalid';
+		?>
+		<label><input type="checkbox" name="cf7_ga_send_actions[<?php echo esc_attr( $action ); ?>]" value="true" <?php checked( 'true', $this->get_send_actions( $action ) ); ?>/><?php echo esc_html( $this->actions[ $action ]['description'] ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Render setting field for spam.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function render_setting_spam() {
+		$action = 'spam';
+		?>
+		<label><input type="checkbox" name="cf7_ga_send_actions[<?php echo esc_attr( $action ); ?>]" value="true" <?php checked( 'true', $this->get_send_actions( $action ) ); ?>/><?php echo esc_html( $this->actions[ $action ]['description'] ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Render setting field for mail_sent.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function render_setting_mail_sent() {
+		$action = 'mail_sent';
+		?>
+		<label><input type="checkbox" name="cf7_ga_send_actions[<?php echo esc_attr( $action ); ?>]" value="true" <?php checked( 'true', $this->get_send_actions( $action ) ); ?>/><?php echo esc_html( $this->actions[ $action ]['description'] ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Render setting field for mail_failed.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function render_setting_mail_failed() {
+		$action = 'mail_failed';
+		?>
+		<label><input type="checkbox" name="cf7_ga_send_actions[<?php echo esc_attr( $action ); ?>]" value="true" <?php checked( 'true', $this->get_send_actions( $action ) ); ?>/><?php echo esc_html( $this->actions[ $action ]['description'] ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Render setting field for sent.
+	 *
+	 * @since 1.8.0
+	 *
+	 * @return void
+	 */
+	public function render_setting_sent() {
+		$action = 'sent';
+		?>
+		<label><input type="checkbox" name="cf7_ga_send_actions[<?php echo esc_attr( $action ); ?>]" value="true" <?php checked( 'true', $this->get_send_actions( $action ) ); ?>/><?php echo esc_html( $this->actions[ $action ]['description'] ); ?></label>
+		<?php
+	}
+
+	/**
+	 * Add admin notice about new tracking behavior.
+	 *
+	 * @since 1.7.0
+	 */
+	public function admin_notices_170() {
+		?>
+		<div class="notice notice-info cf7-ga-notice is-dismissible" data-version="170">
 			<h2>Contact Form 7 to Google Analytics Update</h2>
 			<p>The tracking behavior has <strong>added more events</strong> since version 1.7.0. It now sends data to Google Analytics about <strong>all</strong> form submission attempts. Here is a list of the events you will begin to see since the upgrade:</p>
 			<ul>
@@ -144,9 +377,35 @@ class CF7_Google_Analytics {
 	}
 
 	/**
-	 * Update option for CF7 GA 170 notes
+	 * Update option for CF7 GA 170 notes.
+	 *
+	 * @since 1.7.0
 	 */
 	public function cf7_ga_dismiss_notice_170() {
 		update_option( 'cf7-ga-170-notice-dismissed', 1, false );
 	}
+
+	/**
+	 * Add admin notice about options for disabling events.
+	 *
+	 * @since 1.8.0
+	 */
+	public function admin_notices_180() {
+		?>
+		<div class="notice notice-info cf7-ga-notice is-dismissible" data-version="180">
+			<h2>Contact Form 7 to Google Analytics Update</h2>
+			<p>As requested by many users, you can now choose which events will be sent or ignored. Visit the <a href="<?php echo esc_url( get_admin_url( null, 'options-general.php?page=cf7_ga' ) ); ?>">settings page</a> to choose which events to send.</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Update option for CF7 GA 180 notes.
+	 *
+	 * @since 1.8.0
+	 */
+	public function cf7_ga_dismiss_notice_180() {
+		update_option( 'cf7-ga-180-notice-dismissed', 1, false );
+	}
+
 }
