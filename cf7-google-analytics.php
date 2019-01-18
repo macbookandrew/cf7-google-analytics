d6d8b40857c1abf735af3d584ede227fe4b35fef
Andrew Minion <andrew@andrewrminion.com>
2019-01-17 21:09:53 -0500

    fix typo in old tracking code for GTM

--- cf7-google-analytics.php
+++ cf7-google-analytics.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://andrewrminion.com/contact-form-7-google-analytics/
  * Description: Adds Google Analytics Event Tracking to all Contact Form 7 forms.
  * Tags: contact form, contact form 7, cf7, contactform7, google analytics, ga, universal, forms, form, track, tracking, event, events, goal, goals
- * Version: 1.8.6
+ * Version: 1.8.7
  * Author: Andrew Minion
  * Author URI: https://www.andrewrminion.com
  *
